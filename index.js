1c18aa3ae4234d123bb9b7b7dfa2c3d202de2ace
Oscar Lopez <oscarfrom95@hmail.com>
2020-09-09 22:14:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // add solution here
 var empty = []
+var musicians = []
+var instruments = []
 
-function theBeatlesPlay(arr1, arr2) {
+function theBeatlesPlay(musicians, instruments) {
 
-  for (i = 0; i <= arr1.length; i++) {
-      arr1[0] = "John Lennon"
-      arr2[0] = " plays guitar"
-      empty.push(`arr1[${i}] arr2[${i}]`)
-      returns empty
+  for (i = 0; i <= musicians.length; i++) {
+    if (i === 0) {
+      musicians[0] = "John Lennon"
+      instruments[0] = " plays guitar"
+      empty[0].push(`${musicians[0]}` + `${musicians[0]}`)
+    } else {
+      empty.push()
+    }
+      
     }
 
   }
