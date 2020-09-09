709babc4f05fee927f3f2ce2dc821f1c8aaf9db9
Oscar Lopez <oscarfrom95@hmail.com>
2020-09-09 20:49:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // add solution here
-var empty = [] 
+var empty = []
 
 function theBeatlesPlay(arr1, arr2) {
-  
+
   for (i = 0; i <= arr1.length; i++) {
       arr1[0] = "John Lennon"
       arr2[0] = " plays guitar"
-      empty.push(`arr1[${i}] arr2[${i}]`) 
-      returns empty    
+      empty.push(`arr1[${i}] arr2[${i}]`)
+      returns empty
     }
-    
-  }
 
+  }
