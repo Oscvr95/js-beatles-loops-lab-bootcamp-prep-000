0bb79ddf729c557f06a34aacf8a3004a93f18746
Oscar Lopez <oscarfrom95@hmail.com>
2020-09-10 00:19:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ instruments = ["Guitar", "Bass Guiter", "Lead Guitar", "Drums"]
 function theBeatlesPlay(musicians, instruments) {
   var empty = []
   for (let i = 0; i < musicians.length; i++) {
-    empty.push(`${musicians[i]} ${instruments[i]}`)
+    empty.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return empty
 }
