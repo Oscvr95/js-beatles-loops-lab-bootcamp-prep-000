3479cdfc91d4e0baabc0e8331d7fb3ac7a52fe72
Oscar Lopez <oscarfrom95@hmail.com>
2020-09-10 00:13:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 // add solutions here
 
-musicians = []
-instruments = []
+musicians = ["John Lennon ", "Paul McCartney ", "George Harrison ", "Ringo Starr "]
+instruments = ["plays Guitar", "plays Bass Guiter", "plays Lead Guitar", "plays Drums"]
 
 function theBeatlesPlay(musicians, instruments) {
   var empty = []
   for (let i = 0; i < musicians.length; i++) {
-    musicians[0] = "John Lennon "
-    instruments[0] = "plays guitar"
     empty.push(`${musicians[i]} ${instruments[i]}`)
   }
 }
